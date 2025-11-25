34fb1f8af94b147475860d19ebe3093203f983bc
DiceDiceThrice <diceroouwu@gmail.com>
2025-11-25 23:24:14 +0800

    Adjustment for Database
    New table for capacity and adjustment to pivot and pending table

--- database/migrations/2025_11_21_095104_create_event_guest_pivot_table.php
+++ database/migrations/2025_11_21_095104_create_event_guest_pivot_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->foreignId('event_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             
-            $table->string('status')->default('Confirmed')->after('user_id'); 
+            $table->string('status')->default('Confirmed');
             
             $table->primary(['event_id', 'user_id']);
             $table->timestamps();
